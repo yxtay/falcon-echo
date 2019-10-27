cdc11d342ba4395375102194b6d040dadcb2828e
yuxuan.tay <wyextay@gmail.com>
2019-10-27 19:52:23 +0800

    remove test health

--- tests/test_falcon_app.py
+++ tests/test_falcon_app.py
@@ -4,12 +4,3 @@ def test_root(client):
         "status": "200 OK",
     }
     assert response == expected_response
-
-
-def test_hello(client):
-    response = client.simulate_get("/hello").json
-    expected_response = {
-        "status": "200 OK",
-        "message": "hello",
-    }
-    assert response == expected_response
