faf3e9e1ffb0e34222336b73a635f32217e4eb77
yuxuan.tay <wyextay@gmail.com>
2019-11-06 16:23:24 +0800

    disable stackdriver trace in dev

--- tests/test_falcon_app.py
+++ tests/test_falcon_app.py
@@ -1,4 +1,14 @@
-def test_root(client):
-    response = client.simulate_get("/").json
+import pytest
+
+
+@pytest.mark.parametrize("route", ["/", "/health"])
+def test_health(client, route):
+    response = client.simulate_get(route).json
     expected_response = {"status": "200 OK"}
+    assert response == expected_response
+
+
+def test_hello(client):
+    response = client.simulate_get("/hello").json
+    expected_response = {"status": "200 OK", "message": "hello"}
     assert response == expected_response
